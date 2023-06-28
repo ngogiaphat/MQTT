a5450e92f48cc544b73240eb799410d5c5fabc72
Samuel Seo <ngogiaphat0802@gmail.com>
2023-06-28 23:48:21 +0700

    add + config package

---
+++ setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup
+setup(
+	name = 'your_package_name',
+	version = '1.0',
+	author = 'your_name',
+	description = 'Your package description',
+	packages = ['your_package'],  
+ 	# package list
+	install_requires = [
+		'db-sqlite3',
+		'flask',
+		'requests',
+		'paho-mqtt',
+   	'pycopy-fcntl',
+    'micropython-fcntl'
+	],
+)
+from setuptools import setup
+setup(
+	name = 'your_package_name',
+	version = '1.0',
+	author = 'your_name',
+	description = 'Your package description',
+	packages = ['your_package'],  
+ 	# package list
+	install_requires = [
+		'db-sqlite3',
+		'flask',
+		'requests',
+		'paho-mqtt',
+   	'pycopy-fcntl',
+    'micropython-fcntl'
+	],
+)
+
+# from setuptools import setup
+# setup(
+# 	name = "homework",
+# 	version = "1.0.0",
+# 	packages = ["myproject"],
+# 	install_requires=[
+# 		"flask",
+# 		"paho-mqtt",
+# 		"mysql-connector-python",
+#   	"python-sql",
+# 		"MySQL-python"
+# 	]
+# )
+# from setuptools import setup
+# setup(
+# 	name = "homework",
+# 	version = "1.0.0",
+# 	packages = ["myproject"],
+# 	install_requires = [
+# 		"flask",
+# 		"paho-mqtt",
+# 		"mysql-connector-python",
+# 		"python-sql",
+# 		"MySQL-python"
+# 	]
+# )
